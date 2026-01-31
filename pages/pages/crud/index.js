8620a318a79bfdb55d27c3215d3f1e1476573a3b
Janavi Soni <janavisoni@MacBookPro.home.local>
2026-01-31 17:10:09 -0500

    code change

--- pages/pages/crud/index.js
+++ pages/pages/crud/index.js
@@ -12,7 +12,6 @@ import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
 import { classNames } from 'primereact/utils';
 import React, { useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../../demo/service/ProductService';
 
 const Crud = () => {
     let emptyProduct = {
